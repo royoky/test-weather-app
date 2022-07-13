01831d0cf525ccebfef2a96685acef25490a61d4
Ronan Marzin <ronan.marzin@protonmail.com>
2022-07-13 11:44:08 +0200

    feat: add app bar chore: add netlify config

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <v-app>
+    <v-app-bar
+      color="teal-darken-4"
+      image="http://www.pixelstalk.net/wp-content/uploads/2016/05/Blue-sunny-sky.jpg"
+    >
+      <v-app-bar-title
+        ><div class="app-bar-title">Weather Forecast</div></v-app-bar-title
+      >
+    </v-app-bar>
     <v-main>
       <HomePage />
     </v-main>
@@ -9,3 +17,10 @@
 <script setup lang="ts">
 import HomePage from "./views/HomePage.vue";
 </script>
+
+<style lang="scss">
+.app-bar-title {
+  color: rgb(49, 46, 46);
+  text-align: center;
+}
+</style>
